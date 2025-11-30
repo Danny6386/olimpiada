24d0f81de3655e59db21ab65763ddd6d7164cf20
Daniel <pricedanielblue@gmail.com>
2025-11-30 11:28:09 +0100

    30.11.25

---
+++ reszta.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int k = 0, n = 0, tab[1000000], dp[1000000];
+
+int main(){
+    cin>> k>> n;
+    for (int i = 0; i < n; ++i){
+        cin>> tab[i];
+    }
+
+    sort(tab + n, tab, greater<int>());
+
+    while (k > 0){
+        
+    }
+
+}
